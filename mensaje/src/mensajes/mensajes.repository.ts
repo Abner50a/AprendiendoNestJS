9434e2023bb7a6efa8b4a7317e5d89da693f851f
Abner50a <soporteabner@gmail.com>
2021-07-27 18:17:55 -0500

    Creando repository

--- mensaje/src/mensajes/mensajes.repository.ts
+++ mensaje/src/mensajes/mensajes.repository.ts
@@ -1,11 +1,35 @@
+import { readFile, writeFile } from 'fs/promises';
+
 export class MensajesRepository {
-   async findOne(id: string){}
+  async findOne(id: string) {
+    const contenido = await readFile('mensaje.json', 'utf-8');
+    const mensaje = JSON.parse(contenido);
 
-    async findAll(){
+    return mensaje[id];
+  }
 
-    }
+  async findAll() {
+    const contenido = await readFile('mensaje.json', 'utf-8');
+    const mensaje = JSON.parse(contenido);
 
-    async create(mensaje: string  ) {
-      
-    }
+    return mensaje;
+  }
+
+  async create(contenido: string) {
+    const contenidos = await readFile('mensaje.json', 'utf-8');
+    const mensajes = JSON.parse(contenidos);
+
+    const id = Math.floor(Math.random() * 999);
+
+    mensajes[id] = { id, contenido: contenido };
+
+    await writeFile('mensaje.json', JSON.stringify(mensajes));
+
+    // {
+    //   12 : {
+    //     id: 12,
+    //     content : "hola"
+    //   }
+    // }
+  }
 }
