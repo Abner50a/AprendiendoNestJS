66c228bb2dd13239cc3809d6f4aed8471642c736
Abner50a <soporteabner@gmail.com>
2021-07-27 16:17:19 -0500

    Agregando validationPipe a global en main.ts

--- mensaje/src/main.ts
+++ mensaje/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { MensajesModule } from './mensajes/mensajes.module';
+import { ValidationPipe } from '@nestjs/common';
 
+import { MensajesModule } from './mensajes/mensajes.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MensajesModule);
+
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
