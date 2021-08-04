2a354ec427515d1eeb85c941eb58f0cf234c3612
Abner50a <soporteabner@gmail.com>
2021-08-03 19:38:10 -0500

    Se termino la base de manejo d ela api con su validacion

--- valuo-micarro/src/usuarios/usuarios.controller.ts
+++ valuo-micarro/src/usuarios/usuarios.controller.ts
@@ -1,5 +1,7 @@
-import { Body, Controller, Post,Get,Param,Patch,Query } from '@nestjs/common';
+import { Body, Controller, Post,Get,Param,Patch,Query,Delete } from '@nestjs/common';
 import { CrearUsuarioDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+
 import { UsuariosService } from './usuarios.service';
 
 @Controller('auth')
@@ -9,5 +11,25 @@ export class UsuariosController {
   @Post('/signup')
   crearUsuario(@Body() body: CrearUsuarioDto) {
     this.userService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  finduser(@Param('id') id: string){
+    //Param extrae el valor de la url por ejemplo /:id este va estraer el valor de la id
+    return this.userService.findOne(parseInt(id));
+  }
+
+  @Get()
+  buscarUsuarios(@Query('email') email: string){
+    return this.userService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id')  id:string  ){
+    return this.userService.remove(parseInt(id));
+  }
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body:  UpdateUserDto ){
+    return this.userService.update(parseInt(id), body)
   }
 }
