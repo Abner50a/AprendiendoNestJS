3c77d57f49f6f570cdd2a0012ed22c579b776df8
Abner50a <soporteabner@gmail.com>
2021-08-14 12:41:18 -0500

    Base del siguiente proyecto

---
+++ medium-clone/ormconfig.ts
@@ -0,0 +1,13 @@
+import { ConnectionOptions } from "typeorm";
+
+const config : ConnectionOptions = {
+  type: 'postgres',
+  host: '127.0.0.1',
+  port: 5432,
+  username: 'postgres',
+  password: 'root',
+  database: 'mediumclone'
+
+};
+
+export default config;
